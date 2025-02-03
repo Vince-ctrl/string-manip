7dc5d799c5e59244b1038132aefae701bc31bab7
Vincent Adams <61333244+Vince-ctrl@users.noreply.github.com>
2025-02-03 12:50:23 -0700

    Added the main file

---
+++ App.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+public class App {
+    public static void main(String[] args) throws Exception {
+        //making the input and the variables
+        Scanner input = new Scanner(System.in);
+        String preSplit = "1";
+        int c = 1;
+        int i = 0;
+        //input
+        System.out.println("please enter any text below 20 words");
+        preSplit = input.nextLine();
+        //counting the amount of words
+        while (i < preSplit.length()) {
+            if (preSplit.charAt(i) == ' ' && preSplit.charAt(i + 1) != ' ') {
+                c++;
+            }
+            i++;
+        }
+        //making the varibles for the splitting logic
+        int a = 0;
+        String[] split = new String[c];
+        while (a < c) {
+            split[a] = preSplit.split(" ")[a];
+            System.out.println(split[a]);
+            a++;
+        }
+    }
+}
